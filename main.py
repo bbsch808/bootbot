1203d7a7a57161009349728c60f8b6deffeae088
bbsch808 <bbscholar48@gmail.com>
2025-10-18 15:36:07 -0500

    create functions in main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("greetings boots")
+def get_book_text(path):
+
+    with open(path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def word_counter(file_contents):
+
+    words_list = file_contents.split()
+    words_list_length = len(words_list)
+    return words_list_length
+
+
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    num_words = word_counter(text)
+    print(f"Found {num_words} total words")
+
+main()
