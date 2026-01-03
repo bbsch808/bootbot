03918a9151a5eb5613987e99a0e6d6c8009d25ae
bbsch808 <bbscholar48@gmail.com>
2026-01-03 15:22:51 -0600

    created more book options

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_book_text
 
 from stats import get_letters_numbered
@@ -14,14 +16,19 @@ def word_counter(file_contents):
     return words_list_length
 
 
-
+#prints report
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    try:
+        sys.argv[1]
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
     num_words = word_counter(text)
     dictionary = get_letters_numbered(f"{text}")
     final_dictionary = sort_dictionary(text)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
